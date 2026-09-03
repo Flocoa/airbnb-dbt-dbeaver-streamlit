6bcf9063e24eb558195a62f69f6718c4b8f055d6
florian coadic <florian@coadic.fr>
2026-09-03 10:33:07 +0200

    adding review and calendar tables in ingest.py, and a ipynb file to explore the data'

--- ingest.py
+++ ingest.py
@@ -23,13 +23,30 @@ def ingest_raw_data():
         SELECT * FROM read_csv_auto('data/calendar.csv.gz', ignore_errors=true);
     """)
 
+    print("Ingestion de reviews.csv.gz...")
+    conn.execute("""
+        CREATE OR REPLACE TABLE raw.raw_reviews AS
+        SELECT * FROM read_csv_auto('data/reviews.csv.gz', ignore_errors=true);
+    """)
+
+    print("Ingestion de neighbors.csv..")
+    conn.execute("""
+        CREATE OR REPLACE TABLE raw.raw_neighbors AS
+        SELECT * FROM read_csv_auto('data/neighbors.csv', ignore_errors=true);
+    """)
+
+
     # Vérification
     listings_count = conn.execute("SELECT count(*) FROM raw.raw_listings;").fetchone()[0]
     calendar_count = conn.execute("SELECT count(*) FROM raw.raw_calendar;").fetchone()[0]
+    reviews_count = conn.execute("SELECT count(*) FROM raw.raw_reviews;").fetchone()[0]
+    neighbors_count = conn.execute("SELECT count(*) FROM raw.raw_neighbors;").fetchone()[0]
 
     print(f" Ingestion terminée !")
     print(f"• raw_listings : {listings_count:,} lignes")
     print(f"• raw_calendar : {calendar_count:,} lignes")
+    print(f"• raw_reviews : {reviews_count:,} lignes")
+    print(f"• raw_neighbors : {neighbors_count:,} lignes")
 
     conn.close()
 
